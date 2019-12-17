ba74e787c51c592aae316bbe768b5f680b2b80bf
Kaleb Lucas <klucas765@gmail.com>
2019-12-16 22:02:38 -0600

    Completed base studio and began bonus missions

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./equipment.component.css']
 })
 export class EquipmentComponent implements OnInit {
-
+  equipment: object[] = [
+    {name: "Habitat dome", vital: true},
+    {name: "Drones", vital: false},
+    {name: "Food containers", vital: true},
+    {name: "Oxygen tanks", vital: true}
+  ];
+  itemBeingEdited: object = null;
   constructor() { }
 
   ngOnInit() {
   }
+  add(itemName: string, isVital: boolean) {
+    this.equipment.push({name: itemName, vital: isVital});
+  }
 
+  remove(item: object) {
+    let index = this.equipment.indexOf(item);
+    this.equipment.splice(index, 1);
+  }
+
+  edit(item: object) {
+    this.itemBeingEdited = item;
+ }
+
+ save(name: string, item: object) {
+  item['name'] = name;
+  this.itemBeingEdited = null;
+}
 }
